f12f1a915d991d06a7bea3c54a999d388ab20b20
kaian-moura <193531568+Kaian-Moura@users.noreply.github.com>
2025-11-12 14:51:46 -0300

    chore: publish site in docs/ via Docusaurus

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,41 +3,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/futebolderobo/__docusaurus/debug',
-    component: ComponentCreator('/futebolderobo/__docusaurus/debug', 'cb6'),
-    exact: true
-  },
-  {
-    path: '/futebolderobo/__docusaurus/debug/config',
-    component: ComponentCreator('/futebolderobo/__docusaurus/debug/config', '01b'),
-    exact: true
-  },
-  {
-    path: '/futebolderobo/__docusaurus/debug/content',
-    component: ComponentCreator('/futebolderobo/__docusaurus/debug/content', '2e9'),
-    exact: true
-  },
-  {
-    path: '/futebolderobo/__docusaurus/debug/globalData',
-    component: ComponentCreator('/futebolderobo/__docusaurus/debug/globalData', '892'),
-    exact: true
-  },
-  {
-    path: '/futebolderobo/__docusaurus/debug/metadata',
-    component: ComponentCreator('/futebolderobo/__docusaurus/debug/metadata', '57f'),
-    exact: true
-  },
-  {
-    path: '/futebolderobo/__docusaurus/debug/registry',
-    component: ComponentCreator('/futebolderobo/__docusaurus/debug/registry', 'f81'),
-    exact: true
-  },
-  {
-    path: '/futebolderobo/__docusaurus/debug/routes',
-    component: ComponentCreator('/futebolderobo/__docusaurus/debug/routes', 'b3b'),
-    exact: true
-  },
-  {
     path: '/futebolderobo/',
     component: ComponentCreator('/futebolderobo/', 'ac0'),
     routes: [
